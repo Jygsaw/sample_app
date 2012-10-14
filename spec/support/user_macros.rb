9275c88e2171fc58e683cb3f3a36b99f76e7f0cf
Jygsaw <jygsaw@gmail.com>
2012-10-13 23:57:48 -0400

    Finish user edit, update, index, and destroy actions

--- spec/support/user_macros.rb
+++ spec/support/user_macros.rb
@@ -1,12 +1,15 @@
 module UserMacros
 
-  def valid_signin(user)
+  def sign_in(user)
+    visit signin_path
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button "Sign in"
+    # Sign in when not using Capybara as well.
+    cookies[:remember_token] = user.remember_token
   end
 
-  def valid_signup_info(user)
+  def sign_up(user)
     fill_in "Name", with: user.name
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
